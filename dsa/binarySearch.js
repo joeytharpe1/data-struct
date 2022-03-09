02dbf1d444021fccd6f96ee5e15c8eacefdaedd0
Joseph or Joeyy <solowun6@aol.com>
2022-03-09 21:34:49 +0000

    changed function name

--- dsa/binarySearch.js
+++ dsa/binarySearch.js
@@ -1,9 +1,9 @@
-//Binary search
+//Binary search which is divide and conquer
 //TIME: BIG O(log n)
 
 const num = [9, 11, 13, 15, 20, 27, 30, 32]
 
-const searchLinear = (arr, target) => {
+const searchBinary = (arr, target) => {
 if(arr.length === 0) return -1;
   
   let left = 0;
@@ -25,7 +25,7 @@ if(arr.length === 0) return -1;
 }
 
 
-console.log(searchLinear(num, 27))
+console.log(searchBinary(num, 27))
 
 
 
