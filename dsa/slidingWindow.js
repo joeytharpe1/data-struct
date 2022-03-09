3d6fe89bc45627e4d4ec90ab9b6057655d0af016
Joseph or Joeyy <solowun6@aol.com>
2022-03-09 21:07:14 +0000

    add comments for each line

--- dsa/slidingWindow.js
+++ dsa/slidingWindow.js
@@ -1,18 +1,18 @@
 /* write a function which accepts an array of int and a number called num. the function should calculate the maxium sum of n consecutive elements in the array */
 
 function maxSubArraySum(arr, num){
-  if(num > arr.length) return null;
+  if(num > arr.length) return null; //edge case
   let tempSum = 0;
   let maxSum = 0;
-  for(let i = 0; i < num; i++){
-    maxSum = arr[i];
+  for(let i = 0; i < num; i++){ //loop to add all numbers to maxsum variable
+    maxSum = arr[i]; //maxsum = arr[1]....
   }
-  tempSum = maxSum;
-  for(let i = num; i < arr.length; i++){
-    tempSum = tempSum - arr[i-num] + arr[i];
-    maxSum = Math.max(maxSum, tempSum);
+  tempSum = maxSum; //set maxsum to tempsum
+  for(let i = num; i < arr.length; i++){ // set index to num variable
+    tempSum = tempSum - arr[i-num] + arr[i]; // set new temp variable
+    maxSum = Math.max(maxSum, tempSum); //return the max of maxSum & tempSum
   }
-  return maxSum;
+  return maxSum; //finally return maxSum
 }
 
 
