7fd4eae70a1563e921ac78e9c67c357302184e69
Joseph or Joeyy <solowun6@aol.com>
2022-03-09 19:19:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const num = [9, 11, 13, 15, 20, 27, 30, 32]
+
+const searchLinear = (arr, target) => {
+if(arr.length === 0) return -1;
+  
+  let left = 0;
+  let right = arr.length-1;
+
+  while(left <= right){
+    let middle = Math.floor((left + right) / 2);
+
+    if(arr[middle] > target){
+      right = middle - 1;
+    } else if(arr[middle] < target){
+      left = middle + 1;
+    } else {
+      return middle;
+    }
+    
+  }
+  return -1;
+}
+
+
+console.log(searchLinear(num, 27))
