c2244e4558c0b55878c59c45243bac4fd1ba8740
Joseph or Joeyy <solowun6@aol.com>
2022-03-09 19:48:01 +0000

    change index to pointer

--- dsa/twoPointers.js
+++ dsa/twoPointers.js
@@ -4,17 +4,17 @@ const num = [1,1,2,3,3,4,5,6,6,7]
 
 const countUniqueValues = (arr) => {
  let i = 0; //set index i at 0
-  //loop through arr with second index at 1
+  //loop through arr with second pointer at 1
   for(let j = 1; j < arr.length; j++){
     /* check if arr[i] and arr[j] are not equal 
-      if not then increment the index at 0 and set index at 0(arr[i]) = index at 1 (arr[j])  
+      if not then increment the index at 0 and set pointer at 0(arr[i]) = pointer at 1 (arr[j])  
     */
     if(arr[i] !== arr[j]){
       i++;
       arr[i] = arr[j];
     }
   }
-  //if arr[i] and arr[j] are equal then just add 1 to i
+  //if arr[i] and arr[j] are equal then just add 1 to i pointer
   return i + 1
 }
 
