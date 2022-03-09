a49f06f1a8673ae49f689f5c1df279886bf7c95c
Joseph or Joeyy <solowun6@aol.com>
2022-03-09 23:10:18 +0000

    first commit

---
+++ dsa/duplicates.js
@@ -0,0 +1,34 @@
+//TIME- BIG O(n)
+
+//areThereDuplicates Solution (Frequency Counter)
+function areThereDuplicates() {
+  let collection = {}
+  for(let val in arguments){
+    collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+  }
+  for(let key in collection){
+    if(collection[key] > 1) return true
+  }
+  return false;
+}
+
+//areThereDuplicates Solution (Multiple Pointers)
+function areThereDuplicates(...args) {
+  // Two pointers
+  args.sort((a,b) => a > b);
+  let start = 0;
+  let next = 1;
+  while(next < args.length){
+    if(args[start] === args[next]){
+        return true
+    }
+    start++
+    next++
+  }
+  return false
+}
+
+//areThereDuplicates One Liner Solution
+function areThereDuplicates() {
+  return new Set(arguments).size !== arguments.length;
+}
