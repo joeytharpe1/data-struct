923c653b69b55f17760453bc2e1cd1ec8daf0267
Joseph or Joeyy <solowun6@aol.com>
2022-03-10 20:28:42 +0000

    added comments for how this algorithm works with each call stack

--- dsa/sumRangeRecursion.js
+++ dsa/sumRangeRecursion.js
@@ -7,3 +7,9 @@ function sumRange(num){
 }
 
 console.log(sumRange(3))
+/*
+  return 3 + sumRange(2) bc 3-1 = 2
+  then return 2 + sumRange(1) bc 2-1 = 1
+  finally return our base case which is true because the num(1) does infact
+  equals to 1 which is the base case
+*/
